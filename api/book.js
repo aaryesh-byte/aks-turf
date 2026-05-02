f60502475fc25b5d1e945db7a1b076712df3e222
aaryesh-byte <aaryesh13@gmail.com>
2026-05-02 15:43:56 +0530

    Update book.js

--- api/book.js
+++ api/book.js
@@ -2,7 +2,7 @@ export default async function handler(req, res) {
     if (req.method !== 'POST') return res.status(405).json({ message: 'Only POST' });
 
     // PASTE YOUR NEW GOOGLE APPS SCRIPT WEB APP URL HERE:
-    const GOOGLE_SCRIPT_URL = "YOUR_NEW_WEB_APP_URL";
+    const GOOGLE_SCRIPT_URL = "https://script.google.com/macros/s/AKfycbwf3oZpyyeiaUgPRn2dYAVbucGCFhb0xgtZoJ4c9iB34T5-qbGTBx2Ol6PIVy-OfyKv1Q/exec";
 
     try {
         const response = await fetch(GOOGLE_SCRIPT_URL, {
